79c9844c9a2cc266d2df0f9f47af78c5ee8e0407
Elian Silva <you@example.com>
2023-05-26 17:35:42 -0300

    update user with PUT

--- src/main/java/com/nelioalves/workshopmongo/service/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -32,6 +33,23 @@ public class UserService {
     public void delete(String id) {
         findById(id);
         repo.deleteById(id);
+    }
+
+    public User update(User obj) {
+        try {
+            Optional<User> newUser = repo.findById(obj.getId());
+            User user = newUser.get();
+            updateData(user, obj);
+            return repo.save(user);
+        } catch (NoSuchElementException e) {
+            throw new ObjectNotFoundException("Object not found.");
+        }
+    }
+
+    private void updateData(User user, User obj) {
+        user.setName(obj.getName());
+        user.setEmail(obj.getEmail());
+
     }
 
     public User fromDTO(UserDTO objDTO) {
